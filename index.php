4ca85bf629b0d5856f238b7d72f16176bbb98fea
Josh Robbs <josh@joshrobbs.com>
2023-12-11 15:12:30 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin name: JWR's Admin Menu Styler
+ * Plugin URI: https://joshrobbs.com
+ * Description: A plugin to style the admin menu. Requires Admin Menu Editory and ACF PRO.
+ * Author: Josh Robbs
+ * Author URI: https://joshrobbs.com
+ * Version 0.9.0
+ *
+ * @package JWR_Admin_Menu_Styler
+ */
+
+namespace JWR\Admin_Menu_Styler;
+
+defined( 'ABSPATH' ) || exit;
+
+/*
+	[] Verify ACF and AME installed and active
+	[] create options page
+	[] Add css to style output
+	[] Add js to toggle sections
+*/
