ab4c8da5d2f66411aae2e8c6ee01c73efabd2130
Josh Robbs <josh@joshrobbs.com>
2023-12-11 16:47:50 -0500

    Add requirements check

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 /**
  * Plugin name: JWR's Admin Menu Styler
  * Plugin URI: https://joshrobbs.com
- * Description: A plugin to style the admin menu. Requires Admin Menu Editory and ACF PRO.
+ * Description: A plugin to style the admin menu. <strong>Requires Admin Menu Editory and ACF PRO.</strong>
  * Author: Josh Robbs
  * Author URI: https://joshrobbs.com
  * Version 0.9.0
  *
+ * @since 20231211
+ * @author Josh Robbs <josh@joshrobbs.com>
  * @package JWR_Admin_Menu_Styler
  */
 
@@ -15,8 +17,11 @@ namespace JWR\Admin_Menu_Styler;
 defined( 'ABSPATH' ) || exit;
 
 /*
-	[] Verify ACF and AME installed and active
+	[x] Verify ACF and AME installed and active
 	[] create options page
 	[] Add css to style output
 	[] Add js to toggle sections
 */
+
+require_once 'utilities/utilities.php'; // temporary inclusion of dev utilities.
+require_once 'php/activation.php';
---
+++ php/activation.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Functions related to plugin activation.
+ *
+ * @since 20231211
+ * @author Josh Robbs <josh@joshrobbs.com>
+ * @package JWR_Admin_Menu_Styler
+ */
+
+namespace JWR\Admin_Menu_Styler\Activation;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Verify that ACF and AME are active.
+ *
+ * @return bool True if ACF and AME are active, false otherwise.
+ */
+function verify_ams_dependencies() {
+	$acf_active = is_plugin_active( 'advanced-custom-fields-pro/acf.php' );
+	$ame_active = is_plugin_active( 'admin-menu-editor/menu-editor.php' );
+
+	if ( $acf_active || $ame_active ) {
+		return true;
+	}
+	return false;
+}
+
+/**
+ * Plugin activation hook.
+ *
+ * @return void
+ */
+function activate() {
+	if ( false === verify_ams_dependencies() ) {
+		\deactivate_plugins( 'jwr-admin-menu-styler/index.php' );
+		exit;
+	}
+}
+add_action( 'activate_plugin', __NAMESPACE__ . '\activate' );
+
+
