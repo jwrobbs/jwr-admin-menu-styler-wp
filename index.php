008149cbdd0a6f3607595f78c0978c98cb212032
Josh Robbs <josh@joshrobbs.com>
2023-12-11 19:01:05 -0500

    Style the options page

--- index.php
+++ index.php
@@ -18,10 +18,26 @@ defined( 'ABSPATH' ) || exit;
 
 /*
 	[x] Verify ACF and AME installed and active
-	[] create options page
+	[x] create options page
 	[] Add css to style output
 	[] Add js to toggle sections
 */
 
-require_once 'utilities/utilities.php'; // temporary inclusion of dev utilities.
 require_once 'php/activation.php';
+require_once 'php/options.php';
+
+
+/**
+ * Enqueue admin scripts and styles.
+ *
+ * @return void
+ */
+function enqueue_admin_scripts() {
+	wp_enqueue_style(
+		'jwr-admin-menu-options',
+		plugin_dir_url( __FILE__ ) . 'css/options-page-styles.css',
+		array(),
+		filemtime( plugin_dir_path( __FILE__ ) . 'css/options-page-styles.css' )
+	);
+}
+\add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\enqueue_admin_scripts' );
