369422eeb11bae97971c0a6e299edb73da3c75b4
Josh Robbs <josh@joshrobbs.com>
2024-02-11 17:08:11 -0500

    Fix field names

--- php/options.php
+++ php/options.php
@@ -22,8 +22,8 @@ defined( 'ABSPATH' ) || exit;
 function add_admin_menu_style_settings() {
 	JWR_Plugin_Options::add_tab( 'Admin Menu Styler', 'ams' );
 	JWR_Plugin_Options::add_color_picker_field( 'Header Background Color', 'header_background_color', '#002244', 25 );
-	JWR_Plugin_Options::add_color_picker_field( 'Header Text Color', 'header_ftext_color', '#ffffff', 25 );
+	JWR_Plugin_Options::add_color_picker_field( 'Header Text Color', 'header_text_color', '#ffffff', 25 );
 	JWR_Plugin_Options::add_color_picker_field( 'Active Header Background Color', 'active_header_background_color', '#820000', 25 );
-	JWR_Plugin_Options::add_color_picker_field( 'Active Header Text Color', 'active_header_ftext_color', '#ffffff', 25 );
+	JWR_Plugin_Options::add_color_picker_field( 'Active Header Text Color', 'active_header_text_color', '#ffffff', 25 );
 }
 add_action( 'update_jwr_control_panel', __NAMESPACE__ . '\add_admin_menu_style_settings' );
