2971535f561907b5409aa29c1039266fbc657464
Josh Robbs <josh@joshrobbs.com>
2024-02-11 17:07:58 -0500

    Fix styles - add fallbacks

--- php/menu-styles.php
+++ php/menu-styles.php
@@ -15,30 +15,29 @@ defined( 'ABSPATH' ) || exit;
  * @return void
  */
 function add_styles_to_head() {
-	// [] Improve guard.
 	// Handle ACF not being installed.
-	// Doesn't handle no values.
 
-	if ( ! function_exists( 'acf_add_local_field_group' ) ) {
-		$header_bg     = \get_field( 'admin_menu_colors_header_background_color', 'options' );
-		$header_text   = \get_field( 'admin_menu_colors_header_text_color', 'options' );
-		$a_header_bg   = \get_field( 'admin_menu_colors_active_header_background_color', 'options' );
-		$a_header_text = \get_field( 'admin_menu_colors_active_header_text_color', 'options' );
-	} else {
-		$header_bg     = '#002244';
-		$header_text   = '#ffffff';
-		$a_header_bg   = '#820000';
-		$a_header_text = '#ffffff';
-	}
+	$header_bg     = \get_field( 'header_background_color', 'option' );
+	$header_text   = \get_field( 'header_text_color', 'option' );
+	$a_header_bg   = \get_field( 'active_header_background_color', 'option' );
+	$a_header_text = \get_field( 'active_header_text_color', 'option' );
+
+	$header_bg = '';
+
+	$header_bg     = ( ! empty( $header_bg ) ) ? $header_bg : '#002244';
+	$header_text   = ( ! empty( $header_text ) ) ? $header_text : '#ffffff';
+	$a_header_bg   = ( ! empty( $a_header_bg ) ) ? $a_header_bg : '#820000';
+	$a_header_text = ( ! empty( $a_header_text ) ) ? $a_header_text : '#ffffff';
 
 	$style = <<<EOS
-	<style>
+	<style id='admin-menu-styler'>
 		#adminmenu li[class*="-menu-section-header"] {
 			background-color: {$header_bg};
 			color: {$header_text};
 		}
 
-		#adminmenu li[class*="-menu-section-header"] > a.menu-top:focus {
+		#adminmenu li[class*="-menu-section-header"] > a.menu-top:focus,
+		#adminmenu li[class*="-menu-section-header"] > a.menu-top:hover {
 			background-color: {$a_header_bg};
 			color: {$a_header_text};
 		}
